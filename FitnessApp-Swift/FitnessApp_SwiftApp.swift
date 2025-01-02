5f711e5b92e3de535e40f0689abc0e0b9c293737
Jordan Conklin <jordan@caffeinated.org>
2025-01-02 15:31:23 -0800

    first commit:

---
+++ FitnessApp-Swift/FitnessApp_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp_SwiftApp.swift
+//  FitnessApp-Swift
+//
+//  Created by Jordan on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
